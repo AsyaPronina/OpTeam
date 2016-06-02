d72a8701cc7e7825c96024b9a4dc08aadd9b9bc2
Alexander Weissberg <Alexander Weissberg>
2016-06-03 02:43:54 +0300

    Added few empty classes

---
+++ OpTeamUI/DirectorProjectView/Src/DirectorProjectView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpTeamUI.DirectorProjectView
+{
+    public partial class DirectorProjectView : OpTeamUI.ProjectView.Src.ProjectView
+    {
+        public DirectorProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OpTeamUI/ParticipantProjectView/Src/ParticipantProjectView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpTeamUI.DirectorProjectView
+{
+    public partial class ParticipantProjectView : OpTeamUI.ProjectView.Src.ProjectView
+    {
+        public ParticipantProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
